07732ed44e259742ab3607d951dbf71417a55a64
Henrique Novais <henriquennoliveira@gmail.com>
2018-09-17 15:07:06 -0300

    Add new HelloWorld code using the Express framework for NodeJs

---
+++ NodeJsBasics/app.js
@@ -0,0 +1,23 @@
+//Now, this code is a NodeJs "HelloWorld" using the Express framework
+var express = require('express'); //The express variable stores a function.
+var app = express(); //calling the function stored in the express variable.
+//var http = require('http'); is the equivallent of these declarations above in pure NodeJs
+
+app.get('/', function(req, res) {
+   res.send("<html><body><p>HELLOW WORLD BOYZ</p></body></html>"); 
+});
+
+
+app.get('/politics', function(req, res) {
+    res.send("<html><body><p> THIS IS NEWS ABOUT POLITICS! SHOUT!</p></body></html>"); 
+ });
+
+app.listen(3000 , function(){
+
+    /*A commonly used name for fuctions that call other functions is "callback function". It is important
+     to note that such name is imprecise, a more accurate name would be "call after function".
+    A callback function is a function that is sucessfully called will call another function.*/ 
+
+    console.log("The server is now executing through the Express Framework \n CHEERS BOYZ \n A SMALL STEP FOR MEN, A BIG STEP TOWARDS EMPLOYMENT!");
+
+});
