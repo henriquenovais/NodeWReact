b7f9fb195a80b4d3ac3264907c829094d9a39ba7
Henrique Novais <henriquennoliveira@gmail.com>
2018-09-17 15:09:39 -0300

    Add commenting about the installation process of Express through NPM

--- NodeJsBasics/app.js
+++ NodeJsBasics/app.js
@@ -1,4 +1,5 @@
 //Now, this code is a NodeJs "HelloWorld" using the Express framework
+//To install express in a new project it is very important to do so through: npm install express -save.
 var express = require('express'); //The express variable stores a function.
 var app = express(); //calling the function stored in the express variable.
 //var http = require('http'); is the equivallent of these declarations above in pure NodeJs
