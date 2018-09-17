bfc9ea59fa9b1849c1168163a62087d6f8248729
Henrique Novais <henriquennoliveira@gmail.com>
2018-09-17 13:05:48 -0300

    Add console.log() command for better resposiveness with more commenting

--- NodeJsBasics/create_server.js
+++ NodeJsBasics/create_server.js
@@ -3,9 +3,22 @@ var http = require('http');
 //In essence, this library is used to add some functionc to our server.
 
 var server = http.createServer( function(req, res){
+//req = request
+//res = response
+    var category = req.url;
+
+    if(category == '/politics'){
+        res.end("<html><body><p>Here you can find news about POLITICS </p></body></html>");
+    }else if(category == '/economy'){
+        res.end("<html><body><p>Here you can find news about ECONOMY </p></body></html>")
+    }
     res.end("<html><body><p>Here's my first words through NodeJs</p></body></html>");
 });
 
-server.listen(3000); //The server will be using the logic port 3000
+
+
+server.listen(3000);
+console.log("Now, the server is listening to the port 3000 boya! CAN'T YOU HEAR THE DATA?! ");
+ //The server will be using the logic port 3000
 //In order to listen to this port just connect into:
 // http://localhost:3000
