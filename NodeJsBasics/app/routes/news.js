db67f940221d0675f27528d9fbddc9367ba65ceb
Henrique Novais <henriquennoliveira@gmail.com>
2018-09-19 17:05:40 -0300

    Add MySql database acess through app/routes/news.js

--- NodeJsBasics/app/routes/news.js
+++ NodeJsBasics/app/routes/news.js
@@ -1,7 +1,20 @@
-module.exports = function(app){
+module.exports = function (app) {
 
-app.get("/news", function(req, res) {
-    res.render("articles/politics.ejs");
-  });
-  
+        app.get("/news", function (req, res) {
+
+            var mysql = require('mysql');
+
+            var connection = mysql.createConnection({
+                host: 'localhost',
+                user: 'root',
+                password: '1234',
+                database: 'newsportal'
+            });
+        
+            connection.query('select * from article', function(error, result){
+                res.send(result);
+            });
+            //res.render("articles/politics.ejs");
+        });
+
 }
