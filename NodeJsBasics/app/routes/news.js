9854c46413f13099e0e890a82ab3d501cd812044
Henrique Novais <henriquennoliveira@gmail.com>
2018-09-20 11:27:27 -0300

    Add and integrate module for database connection

--- NodeJsBasics/app/routes/news.js
+++ NodeJsBasics/app/routes/news.js
@@ -1,21 +1,13 @@
-module.exports = function (app) {
+var dbconnection = require("./../../config/dbConnection.js");
 
-        app.get("/news", function (req, res) {
+module.exports = function(app) {
+  app.get("/news", function(req, res) {
+    var connection = dbconnection();
 
-            var mysql = require('mysql');
-
-            var connection = mysql.createConnection({
-                host: 'localhost',
-                user: 'root',
-                password: '1234',
-                database: 'newsportal'
-            });
-        
-            connection.query('select * from article', function(error, result){
-                //res.send(result);
-                res.render("articles/news.ejs", {newsvar: result});
-            });
-            //res.render("articles/politics.ejs");
-        });
-
-}
+    connection.query("select * from article", function(error, result) {
+      //res.send(result);
+      res.render("articles/news.ejs", { newsvar: result });
+    });
+    //res.render("articles/politics.ejs");
+  });
+};
