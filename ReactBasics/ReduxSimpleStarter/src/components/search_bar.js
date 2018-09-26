436b18c24dec0d29d8182c4fce90ebf031c1ffe7
Henrique Novais <henriquennoliveira@gmail.com>
2018-09-26 15:39:22 -0300

    Add input bar that returns its state to console

--- ReactBasics/ReduxSimpleStarter/src/components/search_bar.js
+++ ReactBasics/ReduxSimpleStarter/src/components/search_bar.js
@@ -1,7 +1,31 @@
-import React from 'react'; //All components with JSX have to import the React module.
+import React, { Component } from "react"; //All components with JSX have to import the React module.
 
+/*This is a functional component.*/
+/*
 const SearchBar = () => {
     return <input/>;
 };
+*/
 
+/* Class-based component is component which is able to save and indicate its estate to another components.*/
+/*
+This is an example of class based components:
+class SearchBar extends Component {
+  render() {
+    return <input onChange = { this.onInputChange}/>;
+  }
+
+  onInputChange(event) {
+      console.log(event.target.value);
+      //event.target.value to get the object's value
+
+  }
+}
+*/
+
+class SearchBar extends Component{
+    render(){
+        return <input onChange = { (event) => (console.log(event.target.value))}/>;
+    }
+}
 export default SearchBar;
