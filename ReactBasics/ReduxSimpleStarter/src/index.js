2170fa07427f6b45241354db85e78d069f975a15
Henrique Novais <henriquennoliveira@gmail.com>
2018-10-13 22:48:55 -0300

    Add functional video list to simple React project

--- ReactBasics/ReduxSimpleStarter/src/index.js
+++ ReactBasics/ReduxSimpleStarter/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
 import SearchBar from './components/search_bar';
 
+
 const API_KEY = 'AIzaSyDX3HfmVWqk_NClW4SeTG5F1VxVaYwq39U';
+
 
 
 //Create a new component and this component has to produce some HTML.
@@ -17,16 +22,33 @@ const App = function(){
 }
 */
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar/>
-  </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({ videos }); //{videos}= {videos: videos}
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+
 }
 
-/*Take this component's generated HTML and put it in the page(in the DOM).*/ 
-ReactDOM.render(<App/>, document.querySelector('.container')); //<App/> is a self-closing tag.
+
+/*Take this component's generated HTML and put it in the page(in the DOM).*/
+ReactDOM.render(<App />, document.querySelector('.container')); //<App/> is a self-closing tag.
 
 
 //App is a component class and <App/> is its instance.
